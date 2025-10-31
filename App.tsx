4bfe1b2a90dbd1e3c722ee43af41b4e60fff3540
jeskev18 <l20120951@morelia.tecnm.mx>
2025-10-30 20:41:20 -0600

    [ADD] Autenticacion supabase backend

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
+import { supabase } from './lib/supabase'
+import { StyleSheet, View, Text } from 'react-native';
+import Auth from './components/Auth';
+import { Session } from '@supabase/supabase-js'
+
 
 export default function App() {
+
+  const [session, setSession] = useState<Session | null>(null)
+  
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
     </View>
   );
 }
